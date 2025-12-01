7fc0db2573d54b4c619047fe164658f6182ab4f3
Joshua Rendon <josh.rendon@gmail.com>
2025-12-01 13:15:11 -0600

    Updated pytest configure file to block xdist as workaround for coverage hang bug

--- tests/conftest.py
+++ tests/conftest.py
@@ -6,3 +6,9 @@ from pathlib import Path
 ROOT = Path(__file__).resolve().parents[1]
 if str(ROOT) not in sys.path:
     sys.path.insert(0, str(ROOT))
+
+def pytest_configure(config):
+    # Completely block the xdist plugin from loading
+    config.pluginmanager.set_blocked("xdist")
+    config.pluginmanager.set_blocked("pytest_xdist")
+    config.pluginmanager.set_blocked("pytest-xdist")
